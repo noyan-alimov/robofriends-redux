dd9b9d20961d2b3992760464bef73b69a6f3416d
Noyan Alimov <noyan.alimov@gmail.com>
2020-03-15 21:55:40 +0000

    Updates

---
+++ precache-manifest.cd72fbee7718f145f217c744180441bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19b900a7896532d894820cb341a7bb5",
+    "url": "/robofriends-redux/index.html"
+  },
+  {
+    "revision": "6d6f13d26a36894f4b56",
+    "url": "/robofriends-redux/static/css/main.b9d89a68.chunk.css"
+  },
+  {
+    "revision": "7a0ea3829422bae9b678",
+    "url": "/robofriends-redux/static/js/2.0dea9e80.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/robofriends-redux/static/js/2.0dea9e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6f13d26a36894f4b56",
+    "url": "/robofriends-redux/static/js/main.28152182.chunk.js"
+  },
+  {
+    "revision": "7ff8de73d0fd19f1db27",
+    "url": "/robofriends-redux/static/js/runtime-main.3c023e82.js"
+  }
+]);
